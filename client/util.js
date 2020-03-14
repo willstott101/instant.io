5280d46b05bcd8fb2f80922fdee741c69be98a5a
Will Stott <willstott101@gmail.com>
2020-03-14 20:01:49 +0000

    Fix client HTMl escaping properly

--- client/util.js
+++ client/util.js
@@ -7,8 +7,8 @@ exports.log = function log (item, unsafe) {
   logHeading.style.display = 'block'
 
   var p = document.createElement('p')
-  if (unsafe) p.textContent = item
-  else p.innerHTML = item
+  if (unsafe) p.innerHTML = item
+  else p.textContent = item
   logElem.appendChild(p)
   return p
 }
